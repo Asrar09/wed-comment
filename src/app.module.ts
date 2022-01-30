975e07ece6b961a33a4f516a1a8ed05cb4f32bd8
Asrar09 <aisasrar18@Gmail.com>
2022-01-30 15:13:10 +0800

    update env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommentModule } from './comment/comment.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     CommentModule,
     MongooseModule.forRoot(
       'mongodb+srv://asrarDB:EvykJHs2MBsQl87a@thecampaign.gcmfw.mongodb.net/wed_inv?retryWrites=true&w=majority',
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3100);
+  console.log('process : ', process.env.PORT);
+  console.log('process FULL : ', process.env);
+  await app.listen(parseInt(process.env.PORT) || 3100);
 }
 bootstrap();
